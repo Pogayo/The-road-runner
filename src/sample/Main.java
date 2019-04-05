c86bcb546f4dc57c4751f58fd0794ff3a257b5dc
Pogayo <pogayo17@alustudent.com>
2019-04-05 20:46:19 +0200

    I don't know

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,15 +4,36 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+
+import java.awt.*;
 import java.io.*;
 import java.io.IOException;
+import javafx.scene.layout.GridPane;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import javax.swing.*;
 
 public class Main extends Application {
     public static FileReader fr;
     public static BufferedReader br;
     public static String FILENAME="C:\\Users\\Student\\IdeaProjects\\Perez-Ian\\pp-ii-the-road-runner-perez-ian\\src\\sample_test_input_1.txt";
     public static int[][] matrix;
+
+    //setting variables for the images
+    static String boulderpath="C:\\Users\\Student\\IdeaProjects\\Perez-Ian\\pp-ii-the-road-runner-perez-ian\\Image Files\\boulder.jpg";
+    static String coyetepath="C:\\Users\\Student\\IdeaProjects\\Perez-Ian\\pp-ii-the-road-runner-perez-ian\\Image Files\\coyote.jpg";
+    static String coyetealtpath="C:\\Users\\Student\\IdeaProjects\\Perez-Ian\\pp-ii-the-road-runner-perez-ian\\Image Files\\coyote_alt.jpg";
+
+
+
+
+
+    //functions
     public static void readFile() {
         try {
 
@@ -53,14 +74,80 @@ public class Main extends Application {
             e.printStackTrace();}
     }
 
+      public HBox createImage(String imagePath) throws FileNotFoundException{
+
+
+        String path=imagePath;
+          Image image = new Image(new FileInputStream(path));
+                  //creating image view
+          ImageView imageView = new ImageView(image);
+          //Setting the position of the image
+          //imageView.setX(50);
+          //imageView.setY(25);
+
+          //setting the fit height and width of the image view
+          imageView.setFitHeight(100);
+          imageView.setFitWidth(200);
+          imageView.getStyleClass().add("imageView");
+          HBox image_container = new HBox();
+
+          String style_inner = "-fx-border-color: black;"
+
+                  + "-fx-border-width: 2;"
+
+                  + "-fx-border-style: solid;";
+
+          image_container .setStyle(style_inner);
+          image_container .getChildren().add(imageView);
+
+
+          //Setting the preserve ratio of the image view
+          imageView.setPreserveRatio(true);
+          return image_container ;
+      }
+
+      public ButtonGroup  oneMoveButton(){
+        ButtonGroup buttonGroupOne=new ButtonGroup();
+         //up button
+        Button up=new Button("Move up");
+
+        Button left=new Button("Move left");
+
+        Button down=new Button("Move down"); //down button
+
+          Button right=new Button("Move right"); //right button
+
+
+          buttonGroupOne.getElements();
+
+
+        return  buttonGroupOne;
+      }
     @Override
     public void start(Stage primaryStage) throws Exception {
         readFile();
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        //primaryStage.setScene(new Scene(root, 300, 275));
+        GridPane grid = new GridPane();
+        //grid.setGridLinesVisible(true);//for debug purposes
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+
+        grid.setPadding(new Insets(25, 25, 25, 25));
+        //adding the images
+        grid.add(createImage(boulderpath),0,0);
+        grid.add(createImage(coyetepath),1,0);
+        grid.add(createImage(coyetealtpath),0,1);
+        grid.add(createImage(coyetepath),1,1);
+
+        Scene scene = new Scene(grid, 600, 575);
+        scene.getStylesheets().add(getClass().getResource("app.css").toExternalForm());
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
+
 
 
     public static void main(String[] args) {
